2b9dfdb87fe1e081897b16fbc6a477ec5e12cb7f
Will Morris <34400048+wmorris55@users.noreply.github.com>
2024-07-30 18:15:24 -0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def find_largest_number(arr):
-    largest = arr[0]
-    for num in arr:
-        if num > largest:
-            largest = num
-    return largest
-
-
-find_largest_number([4, 6, 3, -5, 300])
